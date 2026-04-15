fe4db1e2bca733cee2a45c4fd081eafcaf356bbe
veradec <glitchmill.dyj3d@simplelogin.co>
2026-04-15 19:36:03 +0530

    feat: we have terminal scrolling (sort of)

--- kernel.c
+++ kernel.c
@@ -30,6 +30,10 @@ enum vga_color {
 	VGA_COLOR_WHITE = 15,
 		};
 
+#define VGA_WIDTH   80
+#define VGA_HEIGHT  25
+#define VGA_MEMORY  0xB8000
+
 static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg){
   return fg | bg << 4;
 }
@@ -45,20 +49,17 @@ size_t strlen(const char* str){
   return len;
 }
 
-#define VGA_WIDTH   80
-#define VGA_HEIGHT  25
-#define VGA_MEMORY  0xB8000
-
 size_t term_row;
 size_t term_col;
 uint8_t term_color;
 uint16_t* term_buffer = (uint16_t*) VGA_MEMORY;
 
-void term_init(void){
+char term_data[VGA_WIDTH][VGA_HEIGHT];
+
+void term_init(){
   term_row = 0;
   term_col = 0;
   term_color = vga_entry_color(VGA_COLOR_WHITE, VGA_COLOR_BLACK);
-
   for (size_t y = 0; y < VGA_HEIGHT; y++) {
     for (size_t x = 0; x < VGA_WIDTH; x++) {
       const size_t idx = y * VGA_WIDTH + x;
@@ -67,7 +68,6 @@ void term_init(void){
   }
 }
 
-
 void term_setcolor(uint8_t color){
   term_color = color;
 }
@@ -75,23 +75,27 @@ void term_setcolor(uint8_t color){
 void term_putentry(char c, uint8_t color, size_t x, size_t y){
   const size_t idx = y * VGA_WIDTH + x;
   term_buffer[idx] = vga_entry(c, color);
+  
 }
-
+void term_redraw(void);
 void term_putchar(char c ){
+
   if(c == '\n'){
     term_col = 0;
       term_row++;
       return;
   }
+  term_data[(term_col%VGA_WIDTH)][(term_row%VGA_HEIGHT)] = c;
   term_putentry(c, term_color, term_col, term_row);
+
   if(++term_col == VGA_WIDTH){
     term_col = 0;
     if(++term_row == VGA_HEIGHT){
       term_row = 0;
+      term_redraw();
     }
   }
 }
-
 
 void term_write(const char* data, size_t size)
 {
@@ -100,14 +104,30 @@ void term_write(const char* data, size_t size)
   }
 }
 
-
 void term_writestr(const char* data){
   term_write(data, strlen(data));
 }
-
-
+void term_redraw(void)
+{
+    for (size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t idx = y * VGA_WIDTH + x;
+            term_buffer[idx] = vga_entry(' ', term_color);
+        }
+    }
+    
+    for (size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            char c = term_data[x][y];
+            if (c != '\0') {
+                const size_t idx = y * VGA_WIDTH + x;
+                term_buffer[idx] = vga_entry(c, term_color);
+            }
+        }
+    }
+}
 void kernel_main(void)
 {
   term_init();
-  term_writestr("Hello World\nThis\n\tisFoo");
+
 }
